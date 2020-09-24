90a14fcbe65ce66c264888ac81f5aba711235d30
UXDESIGNNAM <63619711+UXDESIGNNAM@users.noreply.github.com>
2020-09-23 17:26:55 -0700

    HW#19 submittal

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+console.log("your index.js file is loaded correctly");
+
+$(".row").css("background-color", "fff");
+
+
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
